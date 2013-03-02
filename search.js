2c3a5a5c8f5a8c7295d5e13c05919199e1a70234
Prajwal A N <prajwal.an92@gmail.com>
2013-03-02 15:39:55 +0530

    Working version... need to have more elegant design...

--- search.js
+++ search.js
@@ -1,20 +1,42 @@
-
+var begin=0;
+var jdata;
+var jlen=0;
 function extraButtonClick(){
 	if($("#masthead-search-term").val() != ""){
 		console.log("The current content is..." + $("#search-results-holder").innerHTML);
 		$("#search-results-holder").empty();
 		var qlink = "https://gdata.youtube.com/feeds/api/videos?q=" + encodeURIComponent($("#masthead-search-term").val());
 		qlink = qlink+"&alt=json";
+		begin = 0;
 		$.get(qlink, function(data) {
-				var jdata = (data.feed).entry;
-				var jlen = jdata.length;
+				jdata = (data.feed).entry;
+				jlen = jdata.length;
 				console.log(jdata);
 				var innerdiv = document.createElement('div');
 				innerdiv.setAttribute("class", "holder-class");
+				var nextdiv = document.createElement('div');
+				nextdiv.setAttribute("class", "next-button-class");
+				var nextbutton = document.createElement('button');
+				nextbutton.setAttribute("id","next-btn");
+				nextbutton.setAttribute("role", "button");
+				nextbutton.setAttribute("class", "display-button");
+				nextbutton.appendChild(document.createTextNode(">>"));
+				nextbutton.onclick=displayFront;
+				nextdiv.appendChild(nextbutton);
+				var previousdiv = document.createElement('div');
+				previousdiv.setAttribute("class", "previous-button-class");
+				var previousbutton = document.createElement('button');
+				previousbutton.setAttribute("id","previous-btn");
+				previousbutton.setAttribute("role", "button");
+				previousbutton.setAttribute("class", "display-button");
+				previousbutton.appendChild(document.createTextNode("<<"));
+				previousbutton.onclick=displayBack;
+				previousdiv.appendChild(previousbutton);
+				$("#search-results-holder").append(previousdiv);
 				var inlink;
 				var inimg;
 				var intitle;
-				for(i=0;i<jlen;i++){
+				for(i=begin;i<begin+6&&i<jlen;i++){
 					intitle = jdata[i].title.$t;
 					inimg = (jdata[i].media$group.media$thumbnail)[3].url;
 					inlink = (jdata[i].link)[0].href;
@@ -30,7 +52,7 @@ function extraButtonClick(){
 					var linkdiv = document.createElement('div');
 					linkdiv.setAttribute("class", "link-class");
 					var resultlink = document.createElement('a');
-					resultlink.setAttribute("href", inlink);
+					resultlink.href = inlink;
 					resultlink.setAttribute("title", intitle);
 					resultlink.appendChild(document.createTextNode(intitle.substring(0,38)));
 					linkdiv.appendChild(resultlink);
@@ -45,13 +67,146 @@ function extraButtonClick(){
 					resultdiv.appendChild(document.createTextNode("No results found.. :("));
 					innerdiv.appendChild(resultdiv);
 				}
+				begin = begin+6;
 				console.log("The current content is..." + $("#search-results-holder").val());
 				$("#search-results-holder").append(innerdiv);
+				$("#search-results-holder").append(nextdiv);
 		});
 	}
 	return false;
 }
 
+
+function displayFront(){
+	if(begin < jlen){
+		$("#search-results-holder").empty();
+		var innerdiv = document.createElement('div');
+		innerdiv.setAttribute("class", "holder-class");
+		var nextdiv = document.createElement('div');
+		nextdiv.setAttribute("class", "next-button-class");
+		var nextbutton = document.createElement('button');
+		nextbutton.setAttribute("id","next-btn");
+		nextbutton.setAttribute("role", "button");
+		nextbutton.setAttribute("class", "display-button");
+		nextbutton.appendChild(document.createTextNode(">>"));
+		nextbutton.onclick=displayFront;
+		nextdiv.appendChild(nextbutton);
+		var previousdiv = document.createElement('div');
+		previousdiv.setAttribute("class", "previous-button-class");
+		var previousbutton = document.createElement('button');
+		previousbutton.setAttribute("id","previous-btn");
+		previousbutton.setAttribute("role", "button");
+		previousbutton.setAttribute("class", "display-button");
+		previousbutton.appendChild(document.createTextNode("<<"));
+		previousbutton.onclick=displayBack;
+		previousdiv.appendChild(previousbutton);
+		$("#search-results-holder").append(previousdiv);
+		var inlink;
+		var inimg;
+		var intitle;
+		if(begin+6 < jlen){
+			begin = begin+6;
+		}
+		for(i=begin;i<begin+6&&i<jlen;i++){
+			intitle = jdata[i].title.$t;
+			inimg = (jdata[i].media$group.media$thumbnail)[3].url;
+			inlink = (jdata[i].link)[0].href;
+			console.log(intitle+ " - "+inlink+" - "+inimg);
+			var resultdiv = document.createElement('div');
+			resultdiv.setAttribute("class", "result-class");
+			var imgdiv = document.createElement('div');
+			imgdiv.setAttribute("class", "img-class");
+			var resultimg = document.createElement('img');
+			resultimg.setAttribute("class", "result-image-class");
+			resultimg.setAttribute("src", inimg);
+			imgdiv.appendChild(resultimg);
+			var linkdiv = document.createElement('div');
+			linkdiv.setAttribute("class", "link-class");
+			var resultlink = document.createElement('a');
+			resultlink.href = inlink;
+			resultlink.setAttribute("title", intitle);
+			resultlink.appendChild(document.createTextNode(intitle.substring(0,38)));
+			linkdiv.appendChild(resultlink);
+			resultdiv.appendChild(imgdiv);
+			resultdiv.appendChild(linkdiv);
+			innerdiv.appendChild(resultdiv);
+		}
+		if(jlen == 0){
+			var resultdiv = document.createElement('div');
+			resultdiv.setAttribute("class", "result-class");
+			resultdiv.appendChild(document.createTextNode("No results found.. :("));
+			innerdiv.appendChild(resultdiv);
+		}
+		$("#search-results-holder").append(innerdiv);
+		$("#search-results-holder").append(nextdiv);
+	}
+}
+
+
+function displayBack(){
+	if(begin < jlen){
+		$("#search-results-holder").empty();
+		var innerdiv = document.createElement('div');
+		innerdiv.setAttribute("class", "holder-class");
+		var nextdiv = document.createElement('div');
+		nextdiv.setAttribute("class", "next-button-class");
+		var nextbutton = document.createElement('button');
+		nextbutton.setAttribute("id","next-btn");
+		nextbutton.setAttribute("role", "button");
+		nextbutton.setAttribute("class", "display-button");
+		nextbutton.appendChild(document.createTextNode(">>"));
+		nextbutton.onclick=displayFront;
+		nextdiv.appendChild(nextbutton);
+		var previousdiv = document.createElement('div');
+		previousdiv.setAttribute("class", "previous-button-class");
+		var previousbutton = document.createElement('button');
+		previousbutton.setAttribute("id","previous-btn");
+		previousbutton.setAttribute("role", "button");
+		previousbutton.setAttribute("class", "display-button");
+		previousbutton.appendChild(document.createTextNode("<<"));
+		previousbutton.onclick=displayBack;
+		previousdiv.appendChild(previousbutton);
+		$("#search-results-holder").append(previousdiv);
+		var inlink;
+		var inimg;
+		var intitle;
+		if(begin-6 >= 0){
+			begin = begin-6;
+		}
+		for(i=begin;i<begin+6&&i<jlen;i++){
+			intitle = jdata[i].title.$t;
+			inimg = (jdata[i].media$group.media$thumbnail)[3].url;
+			inlink = (jdata[i].link)[0].href;
+			console.log(intitle+ " - "+inlink+" - "+inimg);
+			var resultdiv = document.createElement('div');
+			resultdiv.setAttribute("class", "result-class");
+			var imgdiv = document.createElement('div');
+			imgdiv.setAttribute("class", "img-class");
+			var resultimg = document.createElement('img');
+			resultimg.setAttribute("class", "result-image-class");
+			resultimg.setAttribute("src", inimg);
+			imgdiv.appendChild(resultimg);
+			var linkdiv = document.createElement('div');
+			linkdiv.setAttribute("class", "link-class");
+			var resultlink = document.createElement('a');
+			resultlink.href = inlink;
+			resultlink.setAttribute("title", intitle);
+			resultlink.appendChild(document.createTextNode(intitle.substring(0,38)));
+			linkdiv.appendChild(resultlink);
+			resultdiv.appendChild(imgdiv);
+			resultdiv.appendChild(linkdiv);
+			innerdiv.appendChild(resultdiv);
+		}
+		if(jlen == 0){
+			var resultdiv = document.createElement('div');
+			resultdiv.setAttribute("class", "result-class");
+			resultdiv.appendChild(document.createTextNode("No results found.. :("));
+			innerdiv.appendChild(resultdiv);
+		}
+		$("#search-results-holder").append(innerdiv);
+		$("#search-results-holder").append(nextdiv);
+	}
+}
 
 /* Designing the extra search button... */
 var extrabutton = document.createElement('button');
